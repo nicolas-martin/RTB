d8ca8087b445d311ea507bb8dd191fd1c0185f4c
Nicolas Martin <nicolas.martin.0986@gmail.com>
2025-09-26 23:14:06 -0400

    second test

--- quest/data/rtb/validators/busted_round_2_same_card.test.ts
+++ quest/data/rtb/validators/busted_round_2_same_card.test.ts
@@ -51,6 +51,558 @@ describe('busted_round_2_same_card validator', () => {
 		};
 		expect(validate(testData)).toBe(false);
 	});
+	it('should work work with other data', () => {
+		const testData = {
+			"data": {
+				"player": {
+					"games": [
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "QS"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "JD"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "JD"
+								},
+								{
+									"roundIndex": 1,
+									"roundOutcome": "9H"
+								},
+								{
+									"roundIndex": 2,
+									"roundOutcome": "QS"
+								},
+								{
+									"roundIndex": 3,
+									"roundOutcome": "7D"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "9S"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "8C"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "KH"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "8D"
+								},
+								{
+									"roundIndex": 1,
+									"roundOutcome": "3C"
+								},
+								{
+									"roundIndex": 2,
+									"roundOutcome": "3S"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "8S"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "KS"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "AC"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "JH"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "8C"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "5D"
+								},
+								{
+									"roundIndex": 1,
+									"roundOutcome": "7S"
+								},
+								{
+									"roundIndex": 2,
+									"roundOutcome": "7C"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "10S"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "5D"
+								},
+								{
+									"roundIndex": 1,
+									"roundOutcome": "2C"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "7D"
+								},
+								{
+									"roundIndex": 1,
+									"roundOutcome": "7S"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "6H"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "9D"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "AC"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "6S"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "JS"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "4C"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "4C"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "2C"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "2S"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "KD"
+								},
+								{
+									"roundIndex": 1,
+									"roundOutcome": "4H"
+								},
+								{
+									"roundIndex": 2,
+									"roundOutcome": "6S"
+								},
+								{
+									"roundIndex": 3,
+									"roundOutcome": "2H"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "JH"
+								},
+								{
+									"roundIndex": 1,
+									"roundOutcome": "KS"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "8D"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "9H"
+								},
+								{
+									"roundIndex": 1,
+									"roundOutcome": "KC"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "7S"
+								},
+								{
+									"roundIndex": 1,
+									"roundOutcome": "6H"
+								},
+								{
+									"roundIndex": 2,
+									"roundOutcome": "5D"
+								},
+								{
+									"roundIndex": 3,
+									"roundOutcome": "KC"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "8C"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "QC"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "2S"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "3S"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "JH"
+								},
+								{
+									"roundIndex": 1,
+									"roundOutcome": "QH"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "10H"
+								},
+								{
+									"roundIndex": 1,
+									"roundOutcome": "6D"
+								},
+								{
+									"roundIndex": 2,
+									"roundOutcome": "8S"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "7D"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "4S"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "JS"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "AH"
+								},
+								{
+									"roundIndex": 1,
+									"roundOutcome": "3S"
+								},
+								{
+									"roundIndex": 2,
+									"roundOutcome": "9D"
+								},
+								{
+									"roundIndex": 3,
+									"roundOutcome": "2C"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "10D"
+								},
+								{
+									"roundIndex": 1,
+									"roundOutcome": "JH"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "3S"
+								},
+								{
+									"roundIndex": 1,
+									"roundOutcome": "10S"
+								},
+								{
+									"roundIndex": 2,
+									"roundOutcome": "JH"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "8C"
+								},
+								{
+									"roundIndex": 1,
+									"roundOutcome": "10C"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "5S"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "3S"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "10D"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "9S"
+								}
+							]
+						},
+						{
+							"status": "BUSTED",
+							"rounds": [
+								{
+									"roundIndex": 0,
+									"roundOutcome": "KH"
+								}
+							]
+						}
+					]
+				}
+			}
+		};
+
+		expect(validate(testData.data)).toBe(true);
+	});
 
 	it('should work with full test data', () => {
 		const testData = {
