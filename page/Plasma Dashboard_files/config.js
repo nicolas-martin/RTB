5ea3c14a5405db45b131e8f1b113e2af00fe6ef0
Nicolas Martin <nicolas.martin.0986@gmail.com>
2025-09-30 16:32:52 -0400

    delete eco page

--- page/Plasma Dashboard_files/config.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  window._POSTHOG_REMOTE_CONFIG = window._POSTHOG_REMOTE_CONFIG || {};
-  window._POSTHOG_REMOTE_CONFIG['phc_MP5wPzQ9WTpTLMzWuKQxcbC8lktJI64VLRXSayIEi7o'] = {
-    config: {"token": "phc_MP5wPzQ9WTpTLMzWuKQxcbC8lktJI64VLRXSayIEi7o", "supportedCompression": ["gzip", "gzip-js"], "hasFeatureFlags": false, "captureDeadClicks": false, "capturePerformance": {"network_timing": true, "web_vitals": false, "web_vitals_allowed_metrics": null}, "autocapture_opt_out": false, "autocaptureExceptions": false, "analytics": {"endpoint": "/i/v0/e/"}, "elementsChainAsString": true, "errorTracking": {"autocaptureExceptions": false, "suppressionRules": []}, "sessionRecording": {"endpoint": "/s/", "consoleLogRecordingEnabled": true, "recorderVersion": "v2", "sampleRate": null, "minimumDurationMilliseconds": null, "linkedFlag": null, "networkPayloadCapture": null, "masking": {"maskAllInputs": false}, "urlTriggers": [], "urlBlocklist": [], "eventTriggers": [], "triggerMatchType": null, "scriptConfig": null, "recordCanvas": false, "canvasFps": null, "canvasQuality": null}, "heatmaps": false, "surveys": false, "defaultIdentifiedOnly": true},
-    siteApps: []
-  }
-})();
