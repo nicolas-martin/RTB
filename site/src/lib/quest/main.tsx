f01fb9ae9b4d96dd70ec71710a013645ad1256a3
Nicolas Martin <nicolas.martin.0986@gmail.com>
2025-10-06 13:33:41 -0400

    merge cleanup

--- site/src/lib/quest/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import { MetaMaskProvider } from './hooks/useMetaMask'
-
-createRoot(document.getElementById('root')!).render(
-	<MetaMaskProvider>
-		<App />
-	</MetaMaskProvider>
-)
